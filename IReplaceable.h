5d927557b0f02f9e438d83f71c45e6cd171f8514
OsorioTiago <santiago7013@gmail.com>
2020-08-24 17:22:19 -0600

    Clases interfaz prueba

---
+++ IReplaceable.h
@@ -0,0 +1,8 @@
+//
+// Created by santi on 8/24/2020.
+//
+
+#ifndef LAB04_SOLID_IREPLACEABLE_H
+#define LAB04_SOLID_IREPLACEABLE_H
+
+#endif //LAB04_SOLID_IREPLACEABLE_H
---
+++ IShippable.h
@@ -0,0 +1,8 @@
+//
+// Created by santi on 8/24/2020.
+//
+
+#ifndef LAB04_SOLID_ISHIPPABLE_H
+#define LAB04_SOLID_ISHIPPABLE_H
+
+#endif //LAB04_SOLID_ISHIPPABLE_H
